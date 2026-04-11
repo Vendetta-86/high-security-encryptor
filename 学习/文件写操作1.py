bc6fb34e428df5ecd3edbd04a1c7542a662d63e1
Vendetta-86 <wowsword@gmail.com>
2026-04-11 18:27:16 +0800

    Remove unrelated repository content

--- 学习/文件写操作1.py
+++
@@ -1,9 +0,0 @@
-f = open('test.txt', 'w', encoding='utf-8') #打开date.txt文件，若不存在则自动创建，w为覆盖写入而非追加，中文加编码避免报错
-f.write('写入测试\n')
-f.write('write功能在写入中不会自己换行\n')
-f.write('所以要换行必须在句子末尾加上换行符\n')
-f.write('写完要关闭，不然不会存盘\n')
-f.close()
-f = open('test.txt', 'r', encoding='utf-8')
-test1 = f.read()
-print(test1)
--- 学习/文件写操作2.py
+++
@@ -1,8 +0,0 @@
-#with open模式指上下文管理器，保证安全打开关闭文件，无需手动调用close方法，释放资源
-with open('app.txt', 'a', encoding='utf-8') as f: #a模式将内容追加到末尾
-    f.write('追加写入测试\n')
-    f.write('a模式写入到末尾,每运行一次就多写一次\n')
-    f.write('操作记录\n')
-f = open('app.txt', 'r', encoding='utf-8')
-text = f.read()
-print(text)
