c932545de6681088c112f67883d41f7c24eaf132
Vendetta-86 <120008475+Vendetta-86@users.noreply.github.com>
2026-05-25 14:37:21 +0800

    feat: export HSE2 header and wrapper primitives

--- src/high_security_encryptor/hse2/__init__.py
+++ src/high_security_encryptor/hse2/__init__.py
@@ -8,8 +8,10 @@ workflows remain stable while the format is hardened.
 
 from .constants import HSE2_MAGIC, HSE2_VERSION
 from .errors import HSE2Error, HSE2FormatError, HSE2UnsupportedProfileError
+from .header import HSE2Header, dumps_canonical_header, loads_canonical_header
 from .kdf import KdfProfile, get_kdf_profile
 from .keys import KeyBundle, generate_key_bundle
+from .wrappers import WrapperRecord, WrappedKeys, make_placeholder_wrapper
 
 __all__ = [
     "HSE2_MAGIC",
@@ -17,8 +19,14 @@ __all__ = [
     "HSE2Error",
     "HSE2FormatError",
     "HSE2UnsupportedProfileError",
+    "HSE2Header",
     "KdfProfile",
     "KeyBundle",
+    "WrapperRecord",
+    "WrappedKeys",
+    "dumps_canonical_header",
     "generate_key_bundle",
     "get_kdf_profile",
+    "loads_canonical_header",
+    "make_placeholder_wrapper",
 ]
