d74eb6b96b7e073ed050663fc260a8faa2cf425c
Vendetta-86 <120008475+Vendetta-86@users.noreply.github.com>
2026-05-25 15:11:43 +0800

    feat: export HSE2 key primitives

--- src/high_security_encryptor/hse2/__init__.py
+++ src/high_security_encryptor/hse2/__init__.py
@@ -1,5 +1,14 @@
 """HSE2 container model helpers."""
 
+from .keys import (
+    HSE2_KEY_SIZE,
+    HSE2KeyMaterial,
+    generate_dek,
+    generate_kek,
+    generate_key_material,
+    generate_mek,
+    validate_key_bytes,
+)
 from .models import (
     CipherSuite,
     HSE2Header,
@@ -17,11 +26,18 @@ __all__ = [
     "CipherSuite",
     "HSE2Header",
     "HSE2ModelError",
+    "HSE2KeyMaterial",
+    "HSE2_KEY_SIZE",
     "KDFProfile",
     "ManifestPolicy",
     "PayloadLayout",
     "WrappedKeys",
     "WrapperRecord",
     "canonical_json_bytes",
+    "generate_dek",
+    "generate_kek",
+    "generate_key_material",
+    "generate_mek",
     "get_kdf_profile",
+    "validate_key_bytes",
 ]
