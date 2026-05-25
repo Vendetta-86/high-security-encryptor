12750f9dbadf6ac9b47ebc765a7852de68c5267f
Vendetta-86 <120008475+Vendetta-86@users.noreply.github.com>
2026-05-25 15:35:38 +0800

    feat: export HSE2 keyfile KDF helpers

--- src/high_security_encryptor/hse2/__init__.py
+++ src/high_security_encryptor/hse2/__init__.py
@@ -1,6 +1,13 @@
 """HSE2 container model helpers."""
 
 from .encoding import b64decode_bytes, b64encode_bytes
+from .keyfile_kdf import (
+    HSE2_KEYFILE_CONTEXT,
+    HSE2_KEYFILE_MIN_SIZE,
+    KeyfileKDFResult,
+    derive_kek_from_keyfile,
+    validate_keyfile_bytes,
+)
 from .keys import (
     HSE2_KEY_SIZE,
     HSE2KeyMaterial,
@@ -48,11 +55,14 @@ __all__ = [
     "HSE2Header",
     "HSE2ModelError",
     "HSE2KeyMaterial",
+    "HSE2_KEYFILE_CONTEXT",
+    "HSE2_KEYFILE_MIN_SIZE",
     "HSE2_KEY_SIZE",
     "HSE2_KDF_SALT_SIZE",
     "HSE2_WRAP_AUTH_TAG_SIZE",
     "HSE2_WRAP_NONCE_SIZE",
     "KDFProfile",
+    "KeyfileKDFResult",
     "ManifestPolicy",
     "PasswordKDFResult",
     "PayloadLayout",
@@ -64,6 +74,7 @@ __all__ = [
     "b64encode_bytes",
     "build_wrapper_record",
     "canonical_json_bytes",
+    "derive_kek_from_keyfile",
     "derive_kek_from_password",
     "generate_dek",
     "generate_kek",
@@ -74,6 +85,7 @@ __all__ = [
     "normalize_password",
     "unwrap_key_material",
     "validate_key_bytes",
+    "validate_keyfile_bytes",
     "wrap_key_material",
     "wrapped_blob_from_metadata",
     "wrapped_blob_to_metadata",
